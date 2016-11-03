f4c55a979c30488b11604f09888a8aa722d7ab68
Stephanie Riera <stephanie@frontside.io>
2016-11-03 10:59:36 -0500

    Add ability to create tasks

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,19 +1,12 @@
 import Ember from 'ember';
 
-const { get, set } = Ember;
-
 export default Ember.Controller.extend({
-  isDone: false,
   actions: {
-    save() {
-      let newTask = {
-        id: 1,
-        description: this.get('description'),
-        deadline: this.get('deadline'),
-        isDone: false
-      };
-
-      this.get('model').pushObject(newTask);
+    save(description, deadline) {
+      this.get('store').createRecord('task', {
+        description,
+        deadline
+      });
     },
     showAll() {
 
--- app/models/task.js
+++ app/models/task.js
@@ -4,5 +4,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr('string'),
   deadline: DS.attr('string'),
-  isDone: DS.attr('boolean')
+  isDone: DS.attr('boolean', { defaultValue: false })
 });
