91f1921d117d90d9886aad8cdd2d27aeface55a3
Stephanie Riera <stephanie@frontside.io>
2016-11-16 10:29:19 -0600

    Filter tasks with buttons

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isDone: false,
+
+  current: '',
+
+  completed: Ember.computed( 'model', 'current', function() {
+    var tasks = this.get('model');
+    var current = this.get('current');
+
+    let finished = current === 'complete';
+    let unfinished = current === 'incomplete';
+
+    if (finished) {
+      return tasks.filter((task) => {
+        return task.get('isDone') === true;
+      });
+    }
+
+    if (unfinished) {
+      return tasks.filter((task) => {
+        return task.get('isDone') === false;
+      });
+    }
+
+    console.log('tasks');
+    return tasks;
+  }),
+
   actions: {
     save(description, deadline) {
       this.get('store').createRecord('task', {
@@ -9,13 +36,15 @@ export default Ember.Controller.extend({
       }).save();
     },
     showAll() {
-
+      this.set('current', '');
     },
     showCompleted() {
-
+      this.set('current', 'complete');
+      console.log('current');
     },
     showIncomplete() {
-
+      this.set('current', 'incomplete');
+      console.log('current');
     },
     editTask() {
 
